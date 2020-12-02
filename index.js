c3a22d9cc7a53cfb47cfacc191d0f512a7b99ff6
Giuseppe <giuseppemiro@gmail.com>
2020-12-02 16:25:49 +0100

    Fix date comparison, and run every two hours

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const RssReader = require("./src/modules/RssReader");
 const Twitter = require("twitter");
 require("dotenv").config();
 
-const timeToInterval = 6 * 60 * 60 * 1000; // execute every 6 hours.
+const timeToInterval = 2 * 60 * 60 * 1000; // execute every 2 hours.
 const rssXml = process.env.FEED_RSS; //  URL where the feed is.
 let lastTweeted = false;
 
@@ -13,6 +13,14 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
+function start() {
+  readAndTweet();
+
+  setInterval(() => {
+    readAndTweet();
+  }, timeToInterval);
+}
+
 function readAndTweet() {
   const reader = new RssReader(rssXml);
   reader.listen().then((rss) => {
@@ -22,12 +30,25 @@ function readAndTweet() {
   function checkForNewAndTweet(rss) {
     for (i = 0; i < rss.length; i++) {
       const item = rss[i];
-      if (item.date !== lastTweeted.date) {
+      if (isMostRecent(item.date, lastTweeted.date)) {
         return tweet(item);
       }
     }
     console.log("Nothing new!!");
   }
+}
+
+function isMostRecent(a, b) {
+  if (!b) return true;
+
+  const dateA = new Date(a).getTime();
+  const dateB = new Date(b).getTime();
+
+  if (dateA > dateB) {
+    return true;
+  }
+
+  return false;
 }
 
 function tweet(item) {
@@ -43,8 +64,4 @@ function tweet(item) {
   );
 }
 
-readAndTweet();
-
-setInterval(() => {
-  readAndTweet();
-}, timeToInterval);
+start();
